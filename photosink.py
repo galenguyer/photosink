57f5ea15646fb094f2324f533efc850185962f66
Galen Guyer <galen@galenguyer.com>
2021-03-31 17:40:52 -0400

    Start directory traversal

--- photosink.py
+++ photosink.py
@@ -1,5 +1,21 @@
-def main():
-    print("Hello, World!")
+import pathlib
+from pathlib import Path
+
+
+def main() -> None:
+    root_path = Path("./albums")
+    traverse_dir(root_path)
+
+
+def traverse_dir(path: Path) -> None:
+    print(str(path))
+    # Traverse child directories
+    for node in [node for node in path.iterdir() if node.is_dir()]:
+        if node.is_dir():
+            traverse_dir(node)
+    # Handle images
+    for node in [node for node in path.iterdir() if not node.is_dir()]:
+        print(node)
 
 
 if __name__ == "__main__":
