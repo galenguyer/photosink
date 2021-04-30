be596e633c30f3e51db3b3d2b2f517e57ded89fd
Galen Guyer <galen@galenguyer.com>
2021-04-30 19:51:17 -0400

    Add file copying and path creation

--- photosink.py
+++ photosink.py
@@ -1,22 +1,40 @@
+import re
+import os
 import pathlib
+import shutil
 from pathlib import Path
 
 
+ROOT_PATH = Path().cwd().resolve()
+ALBUM_PATH = Path(ROOT_PATH, "albums")
+OUTPUT_PATH = Path("./_site").resolve()
+
+
 def main() -> None:
-    root_path = Path("./albums")
+    root_path = Path(ALBUM_PATH)
     traverse_dir(root_path)
 
 
 def traverse_dir(path: Path) -> None:
-    print(str(path))
     # Traverse child directories
+    out_path = str(path).replace(str(ALBUM_PATH), str(OUTPUT_PATH))
+    if not os.path.exists(out_path):
+        print(f'Creating {out_path}')
+        os.makedirs(out_path)
     for node in [node for node in path.iterdir() if node.is_dir()]:
-        if node.is_dir():
-            traverse_dir(node)
+        traverse_dir(node)
     # Handle images
     for node in [node for node in path.iterdir() if not node.is_dir()]:
-        print(node)
+        resize_image(node, Path(out_path))
 
+
+def resize_image(image: Path, out_path: Path) -> None:
+    if re.search("\.jpe?g$", str(image), re.IGNORECASE):
+        original_path = str(image)
+        site_path = str(Path(out_path, image.name))
+        if not os.path.exists(site_path) or os.path.getmtime(original_path) > os.path.getmtime(site_path):
+            print(f'Copying {original_path} -> {site_path}')
+            shutil.copyfile(original_path, site_path)
 
 if __name__ == "__main__":
     main()
