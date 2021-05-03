466eb7b4907498b3d3a0bdda999a9d5dc999b949
Galen Guyer <galen@galenguyer.com>
2021-05-02 21:17:34 -0400

    Add smaller thumbnails for faster loading

--- photosink.py
+++ photosink.py
@@ -5,7 +5,7 @@ import pathlib
 import shutil
 import random
 from pathlib import Path
-
+from PIL import Image
 from jinja2 import Environment, FileSystemLoader
 
 ROOT_PATH = Path().cwd().resolve()
@@ -35,24 +35,37 @@ def traverse_dir(path: Path):
     # Handle images
     for node in [node for node in path.iterdir() if not node.is_dir()]:
         if re.search("\.jpe?g$", str(node.name), re.IGNORECASE):
-            resize_image(node, Path(out_path))
-            dir["images"].append(node.name)
-    dir["thumb"] = dir["images"][0]
+            dir["images"].append(resize_image(node, Path(out_path)))
+    dir["thumb"] = dir["images"][0] if len(dir["images"]) > 0 else ""
     with open(str(Path(out_path, "index.html")), "w") as fd:
         fd.write(j2env.get_template("index.html").render(name = dir["name"], albums=dir["albums"], images=dir["images"]))
     return dir
 
 
-def resize_image(image: Path, out_path: Path) -> None:
+def resize_image(image: Path, out_path: Path):
     if re.search("\.jpe?g$", str(image), re.IGNORECASE):
         original_path = str(image)
         site_path = str(Path(out_path, image.name))
-        if not os.path.exists(site_path) or os.path.getmtime(
-            original_path
-        ) > os.path.getmtime(site_path):
+        thumb_path = f"{Path(out_path, image.name).parent}/{image.stem}.thumb.jpg"
+        if not os.path.exists(site_path) or os.path.getmtime(original_path) > os.path.getmtime(site_path):
             print(f"Copying {original_path} -> {site_path}")
             shutil.copyfile(original_path, site_path)
-
+        if not os.path.exists(thumb_path) or os.path.getmtime(original_path) > os.path.getmtime(thumb_path):
+            print(f"Resizing {original_path} -> thumbnail")
+            original_image = Image.open(original_path)
+            ratio = original_image.size[1] / original_image.size[0]
+            (width, height) = original_image.size
+            thumb = original_image
+            if width > height:
+                thumb = thumb.crop(((int((width-height)/2)), 0, (int((width+height)/2)), height))
+            elif height > width: 
+                thumb = thumb.crop((0, (int((height-width)/2)), width, (int((height+width)/2))))
+            thumb = thumb.resize((512, 512), Image.ANTIALIAS)
+            try:
+                thumb.save(thumb_path, exif=original_image.info['exif'])                
+            except:
+                thumb.save(thumb_path)
+        return {"path": image.name, "thumb": f"{image.stem}.thumb.jpg"}
 
 if __name__ == "__main__":
     main()
